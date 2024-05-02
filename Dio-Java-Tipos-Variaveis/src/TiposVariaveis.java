b26635d31aa04936a307da8d987532b5ddc6fa41
Andre <doria_95@hotmail.com>
2024-05-02 19:43:16 -0300

    teste

--- Dio-Java-Tipos-Variaveis/src/TiposVariaveis.java
+++ Dio-Java-Tipos-Variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+         System.out.println("Hello, World!");
+
+        int numero = 5;
+        numero = numero + 1;
+        numero++;
+
+
+
     }
 }
---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,11 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTV aparelho = new SmartTV();
+
+        System.out.println(aparelho.ligada);
+        System.out.println(aparelho.canal);
+        System.out.println(aparelho.volume);
+
+        
+    }
+}
