e3b097e1a1ecdf0a52677421306d041bfe25b6f8
Andre <doria_95@hotmail.com>
2024-05-02 20:23:56 -0300

    testes de metodos

--- sistema-smart-tv/src/SmartTV.java
+++ sistema-smart-tv/src/SmartTV.java
@@ -3,4 +3,30 @@ public class SmartTV {
     int canal = 1;
     int volume = 25;
     
+    public void ligar(){
+        this.ligada=true;
+    }
+
+    public void desligar(){
+        this.ligada=false;
+    }
+
+    public void aumentarVol(){
+        this.volume++;
+    }
+    public void diminuirVol(){
+        this.volume--;
+    }
+
+    public void mudarDeCanal(int novoCanal){
+        this.canal = novoCanal;
+    }
+
+    public void aumentarCanal(){
+        this.canal++;
+    }
+
+    public void diminuirCanal(){
+        this.canal--;
+    }
 }
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -2,10 +2,18 @@ public class Usuario {
     public static void main(String[] args) throws Exception {
         SmartTV aparelho = new SmartTV();
 
-        System.out.println(aparelho.ligada);
         System.out.println(aparelho.canal);
-        System.out.println(aparelho.volume);
+        aparelho.aumentarCanal();
+        System.out.println(aparelho.canal);
+        aparelho.aumentarCanal();
+        System.out.println(aparelho.canal);
+        aparelho.aumentarCanal();
+        System.out.println(aparelho.canal);
+        aparelho.diminuirCanal();
+        System.out.println(aparelho.canal);
+        aparelho.mudarDeCanal(87);
+        System.out.println(aparelho.canal);
 
-        
+       
     }
 }
